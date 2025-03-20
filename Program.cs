2e4bd46201e9ddbfa6a15b9072779956dfd43950
jaimeescartinsv <a27421@svalero.com>
2025-03-20 13:40:59 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using StringManager_API.Data;
+using StringManager_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+// Add services to the container
 builder.Services.AddControllers();
+
+// Configure DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StringManagerDb")));
+
+// Configure Dependency Injection for Services
+builder.Services.AddScoped<IPlayerService, PlayerService>();
+builder.Services.AddScoped<IRacquetService, RacquetService>();
+builder.Services.AddScoped<IStringTypeService, StringTypeService>();
+builder.Services.AddScoped<IStringerService, StringerService>();
+builder.Services.AddScoped<ITournamentService, TournamentService>();
+builder.Services.AddScoped<IStringJobService, StringJobService>();
+builder.Services.AddScoped<IDashboardService, DashboardService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
